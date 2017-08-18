aff5a0a6d2a380bab6610f4c52b53fe8a8fe9f6d
iuniX <guilaudensack@hotmail.com>
2017-08-18 10:21:32 -0300

    #Henrique

--- pokeballs.lua
+++ pokeballs.lua
@@ -1,33 +1,22 @@
 local BALLS_OPCODE = 166
 
 local p_pokemonsInfo = {}
+local p_pokeballTopMenuButton
 local p_pokeballsWindow
 local p_pokemonsList
-local p_preSortType
-local p_sortOrder
-local p_loadingPanel
 
-local p_pokeballTopMenuButton
-local _startWindow
-
+local _parseStartReceiveData
+local _parseReceiveData
+local _parseFinishReceiveData
 
 function init()
-  p_pokeballTopMenuButton = modules.client_topmenu.addRightGameButton('pokeballButton', tr('Contador de Pokeballs'), 'images/button', toggle, true)
+  p_pokeballTopMenuButton = modules.client_topmenu.addRightGameButton('pokeballButton', tr('Used Pokeballs'), 'images/button', toggle, true)
 
-  p_pokeballsWindow = g_ui.displayUI('pokeballs')
-  
+  p_pokeballsWindow = g_ui.loadUI('pokeballs', rootWidget)
   p_pokemonsList = p_pokeballsWindow:getChildById('pokemonsList')
-  p_loadingPanel = p_pokeballsWindow:getChildById("loadingPanel")
-  showAllCheckBox = p_pokeballsWindow:getChildById("showAllCheckBox")
-  connect(showAllCheckBox, {onCheckChange = onShowAllChecked()})
-  showAllCheckBox:setChecked(true)
-  p_preSortType = "id"
-  p_sortOrder = "asc"
   
-  g_game.handleExtended(BALLS_OPCODE, receiveData)  
+  g_game.handleExtended(BALLS_OPCODE, receiveData)
   connect(g_game, { onGameEnd = hide})
-  
-  hide()
 end
 
 function terminate() 
@@ -47,7 +36,7 @@ function hide()
   p_pokeballsWindow:hide()
 end
 
-function toggle (var)
+function toggle()
   if p_pokeballsWindow:isVisible() then
     p_pokeballsWindow:hide()
   else
@@ -56,137 +45,126 @@ function toggle (var)
 end
 
 function receiveData(t)  
-  if type(t) == "number" then
-    if t == 1 then
-      p_pokemonsInfo = {}
-      show()
-      toggleLoading(true)
-      return
-    end
-    toggleLoading(false)
-    return
+  if t == 1 then
+    _parseStartReceiveData()
+  elseif t == 2 then
+    _parseFinishReceiveData()
+  else
+    _parseReceiveData(t)
   end
+end
 
-  local currentIndex = #p_pokemonsInfo
+function _parseStartReceiveData()
+  p_pokemonsInfo = {}
+  show()
+  toggleLoading(true)
+end
 
-  for i = 1,#t do
-    local info = t[i]
-    local special = info[3] or 0
-    currentIndex = currentIndex + 1
-    p_pokemonsInfo[currentIndex] = {}
-    p_pokemonsInfo[currentIndex].balls = info[2]
-    p_pokemonsInfo[currentIndex].id = info[1]
-    p_pokemonsInfo[currentIndex].special = special
-    p_pokemonsInfo[currentIndex].name = info[4]
-    p_pokemonsInfo[currentIndex].total = 0
-    p_pokemonsInfo[currentIndex].price = info[5]
-    p_pokemonsInfo[currentIndex].waste = info[6]
+function _parseReceiveData(t)
+  for _,info in ipairs(t) do
+    local pokeInfo = {}
+    pokeInfo.id = info[1]
+    pokeInfo.balls = info[2]
+    pokeInfo.shinyId = info[3] or 0
+    pokeInfo.name = info[4]
+    pokeInfo.price = info[5]
+    pokeInfo.waste = info[6]
 
-    for ballID, value in pairs(p_pokemonsInfo[currentIndex].balls) do
-      p_pokemonsInfo[currentIndex].total = p_pokemonsInfo[currentIndex].total + value
+    pokeInfo.total = 0
+    for _, value in pairs(pokeInfo.balls) do
+      pokeInfo.total = pokeInfo.total + value
     end
+
+    table.insert(p_pokemonsInfo, pokeInfo)
   end
-  refreshData() 
+  refreshData("id", "asc")
+end
+
+function _parseFinishReceiveData()
+  toggleLoading(false)
 end
 
 function toggleLoading(show)
-  p_loadingPanel:setVisible(show)
+  local loadingPanel = p_pokeballsWindow:getChildById("loadingPanel")
+  loadingPanel:setVisible(show)
   p_pokeballsWindow:getChildById("searchText"):setEnabled(not show)
 end
 
-function refreshData(orderType)
-  if p_pokemonsList then
-    cleanPokemons()
-    
-    local reverse = false 
-    if not p_preSortType then
-      p_preSortType = "id"
-    end
-  
-    if not orderType then 
-      orderType = p_preSortType
+function refreshData(orderType, sortOrder)
+  if not sortOrder then
+    if p_pokemonsList.orderType == orderType then
+      sortOrder = p_pokemonsList.sortOrder == "asc" and "desc" or "asc"
     else
-      reverse = (p_preSortType == orderType) and (p_sortOrder == "asc")
+      sortOrder = "asc"
     end
-
-    if reverse then
-      table.sort(p_pokemonsInfo, function( a,b ) return a[orderType] > b[orderType] end )
-    else
-      table.sort(p_pokemonsInfo, function( a,b ) return a[orderType] < b[orderType] end )
-    end
-   
-    for _, value in ipairs(p_pokemonsInfo) do
-       addData(value)
-    end
-    
-    if reverse then
-      p_sortOrder = "desc"
-    else
-      p_sortOrder = "asc"
-    end
-    p_preSortType = orderType
-    
   end
+
+  if sortOrder == "desc" then
+    table.sort(p_pokemonsInfo, function(a, b) return a[orderType] > b[orderType] end)
+  else
+    table.sort(p_pokemonsInfo, function(a, b) return a[orderType] < b[orderType] end)
+  end
+ 
+  p_pokemonsList:destroyChildren()
+  for _, value in ipairs(p_pokemonsInfo) do
+    addData(value)
+  end
+
+  p_pokemonsList.orderType = orderType
+  p_pokemonsList.sortOrder = sortOrder
 end
 
 function addData(data)
-  if data.total == 0 then
-    return
-  end
-
   local currentPokemon = g_ui.createWidget('PokemonData', p_pokemonsList)
-  currentPokemon:setId(data.name)
 
   local id = string.format("%03d", data.id)
-
-  if data.special > 0 then
-    id = tostring(id .. "." .. data.special)
+  if data.shinyId > 0 then
+    id = id .. "." .. data.shinyId
   end
 
   local image = currentPokemon:getChildById("dataImage")
   image:setImageSource("/game_pokedex/pokemons/" .. id)
-  image:setTooltip(tr("Npc Price: $") .. data.price .. tr("\nTotal Spent: $") .. data.waste)
+  image:setTooltip(tr("Npc Price: $%d\nTotal Spent: $%d", data.price, data.waste))
   
   currentPokemon:getChildById("dataName"):setText(data.name)
   currentPokemon:getChildById("dataNumber"):setText("#".. string.format("%03d", data.id))
   currentPokemon:getChildById("dataTotal"):setText(data.total)
 
+  local pokeballsPanel = currentPokemon:getChildById("dataPokeballs")
+  local showAllCheckBox = p_pokeballsWindow:getChildById("showAllCheckBox")
   for ballID = 1,18 do
-    if showAllCheckBox:isChecked() or data.balls[ballID] then
-      local pokeballsPanel = currentPokemon:getChildById("dataPokeballs")
-      local currentPokeball = g_ui.createWidget('PokeballWidget', pokeballsPanel)
-      currentPokeball:setId(ballID)
-      local pbImage = currentPokeball:getChildById("pokeballImage")
-      pbImage:setImageSource("images/pb" .. ballID)
-      local balls = data.balls[ballID] or 0
-      currentPokeball:getChildById("pokeballCount"):setText(balls)   
-   end
+    local currentPokeball = g_ui.createWidget('PokeballWidget', pokeballsPanel)
+    local pbImage = currentPokeball:getChildById("pokeballImage")
+    pbImage:setImageSource("images/pb" .. ballID)
+    local balls = data.balls[ballID] or 0
+    currentPokeball:getChildById("pokeballCount"):setText(balls)
+
+    if not showAllCheckBox:isChecked() and balls == 0 then
+      currentPokeball:setVisible(false)
+    end
   end
 end
 
 function onSearchTextChange(text)
-  if #text <= 0 then return refreshData() end
-  cleanPokemons()
   text = string.lower(text)
-  for _, value in ipairs(p_pokemonsInfo) do
-    if string.find(string.lower(value.name), text) then
-      addData(value)
-    end
+
+  for _,pokemonWidget in ipairs(p_pokemonsList:getChildren()) do
+    local nameWidget = pokemonWidget:getChildById("dataName")
+    local match = #text == 0 or string.find(string.lower(nameWidget:getText()), text)
+    pokemonWidget:setVisible(match)
   end
 end
 
-function cleanPokemons()
-  if p_pokemonsList then
-    local children = p_pokemonsList:recursiveGetChildren()
-    for _, child in ipairs(children) do
-      child:destroy()
-    end
+function onShowAllChecked(showAll)
+  if not p_pokemonsList then
+    return
   end
-end
 
-function onShowAllChecked()
-  cleanPokemons()
-  for _, value in ipairs(p_pokemonsInfo) do
-    addData(value)
+  for _,pokemonWidget in ipairs(p_pokemonsList:getChildren()) do
+    local pokeballsPanel = pokemonWidget:getChildById("dataPokeballs")
+    for _,pokeballWidget in ipairs(pokeballsPanel:getChildren()) do
+      local count = tonumber(pokeballWidget:getChildById("pokeballCount"):getText())
+      pokeballWidget:setVisible(showAll or count > 0)
+    end
   end
 end
